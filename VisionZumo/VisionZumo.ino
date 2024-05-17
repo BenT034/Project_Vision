a3ae3a597a5b407ce557a53961ed2fa41a694f5d
RichVoo <ra.voogd@student.avans.nl>
2024-05-17 14:27:38 +0200

    added switch case voor alternatieve functionaliteit zumo

--- VisionZumo/VisionZumo.ino
+++ VisionZumo/VisionZumo.ino
@@ -14,26 +14,55 @@ bool sended = 0;
 bool pidDriveBool = 0;
 int time =0;
 int time_last    =0;
-
+int baseSpeed = 60; // set base motor speed
 // Functie voor het rijden met PID controller
 void pidDrive()
 {
- 
   time = millis() - time_last;
-  int error = 188 - val; // calculate error
-  float P= 0.50*error; // error correction for P 0.31 GOED
-  I = I + ( time * error * 0.000); //0.00013 GOED
-  float D= (0*((error-last_error))/time); // calculate error correction for K 15 GOED
-  int speed = 55; // set motorspeed
-  motors.setSpeeds(speed+(P+I+D),speed-(P+I+D));
+  int error = 210 - val; // calculate error
+  float P = 0.25 * error; // error correction for P 0.31 GOED
+  I = I + (time * error * 0.000); // 0.00013 GOED
+  float D = (20 * ((error - last_error)) / time); // calculate error correction for K 15 GOED
+  
+  int pidCorrection = (P + I + D);
+  int motor1, motor2;
+  motor1 = baseSpeed + pidCorrection;
+  motor2 = baseSpeed - pidCorrection;
+  // Check if one of the speeds is below 28
+//  Serial.print("voor ");
+//  Serial.print(motor1);
+//  Serial.println(motor2);
+//  if (motor2 < 30) {
+//    // If so, calculate the difference between 28 and the current speed
+//    int speedDiff = 30 - motor2;
+//    // Add the difference to both speeds
+//    motor1 += speedDiff;
+//    motor2 = 30;
+//  }
+//  Serial.print("na1 ");
+//  Serial.print(motor1);
+//  Serial.println(motor2);
+//  if(motor1 < 30)
+//  {
+//     // If so, calculate the difference between 28 and the current speed
+//    int speedDiff = 30 - (motor1);
+//    // Add the difference to both speeds
+//    motor2 += speedDiff;
+//    motor1 = 30;
+//  }
+//  Serial.print("na2 ");
+//  Serial.print(motor1);
+//  Serial.println(motor2);
+  motors.setSpeeds(motor1, motor2);
+  //display.clear();
+//  display.print(motor1);
+//  display.print(motor2);
+//  display.print(pidCorrection);
   last_error = error;
   time_last = millis();
- 
 }
- 
 
 void readByte();
-
 void setup()
 {
   pinMode(toNicla, OUTPUT);
@@ -42,39 +71,42 @@ void setup()
   display.clear();
   display.print(F("Press A"));
   display.clear();
-  
-  //Voor het kalibreren van de lijnen, maar werkt nog niet
-  motors.setSpeeds(100,-100);
-  delay(300);
-  motors.setSpeeds(0,0);
-  do
-  sended = loopFunction();
-  while (!sended);
-  sended = 0;
-  delay(1000);
-  motors.setSpeeds(-100,100);
-  delay(600);
-  motors.setSpeeds(0,0);
-  do
-  sended = loopFunction();
-  while (!sended);
-  sended = 0;
-  delay(1000);
-  motors.setSpeeds(100,-100);
-  delay(300);
-  motors.setSpeeds(0,0);
-  do
-  sended = loopFunction();
-  while (!sended);
-  sended = 0;
+//  //Voor het kalibreren van de lijnen, maar werkt nog niet
+//  motors.setSpeeds(100,-100);
+//  delay(300);
+//  motors.setSpeeds(0,0);
+//  do
+//  sended = loopFunction();
+//  while (!sended);
+//  sended = 0;
+//  delay(1000);
+//  motors.setSpeeds(-100,100);
+//  delay(600);
+//  motors.setSpeeds(0,0);
+//  do
+//  sended = loopFunction();
+//  while (!sended);
+//  sended = 0;
+//  delay(1000);
+//  motors.setSpeeds(100,-100);
+//  delay(300);
+//  motors.setSpeeds(0,0);
+//  do
+//  sended = loopFunction();
+//  while (!sended);
+//  sended = 0;
   pidDriveBool = 1;
+  int counter = 0;
 }
-
 //Reading 0 means a 1 from the nicla
 void loop() {
+  if(counter > 10){                   //if the counter is bigger than 10 the basespeed is set to the normal 60
+    counter = 0;
+    baseSpeed = 60;
+  }
+  counter++;
   loopFunction();
 }
-
 bool loopFunction()
 {
   NiclaWantsToSend = false;
@@ -82,49 +114,96 @@ bool loopFunction()
   {
   //  Serial.println("Nicla wants to send");
     delay(100);
+    Serial.print("Nicla wants to send");
     NiclaWantsToSend = true;
   }
   if(NiclaWantsToSend) //bool so that the robot won't drive during setup
   {
     readByte();
     digitalWrite(toNicla, LOW);
-    if (pidDriveBool)
+    if (pidDriveBool){
       pidDrive();
+    }
+    else{
+      switch(val){
+        case 1:                       //if a stop sign is detected the robot will stand still for a second and then it continues driving
+          motors.setSpeeds(0,0);
+          delay(1000);
+          motors.setSpeeds(60,60);
+          delay(500);
+          break;
+        case 2:                       //if the sign of dead end is seen the robot will turn around
+          motors.setSpeeds(-50,50);
+          delay(400);                 //fine tuning needed for turn around delay
+          motors.setSpeeds(0,0);
+          break;
+        case 3:                       //if the robot sees it is on a priority road it will speed up
+          baseSpeed = 80;
+          counter = 0;
+          break;
+        case 4:                       //the robot sees a sign that it cannot park here but it can stand still so it will move to the right of the road and stand still
+          for(int i = 0; i < 5;i++){  //the loop is used to alter the normal pid drive so the robot moves to the right
+            NiclaWantsToSend = false;
+          if(digitalRead(fromNicla)==0 && NiclaWantsToSend != true) //If Nicla wants to send data
+          {
+          //  Serial.println("Nicla wants to send");
+          delay(100);
+          Serial.print("Nicla wants to send");
+          NiclaWantsToSend = true;
+          }
+          if(NiclaWantsToSend) //bool so that the robot won't drive during setup
+          {
+            readByte();
+            val = val - 90;           //this line of code cause the error to be 90 smaller so the robot moves to the right
+            digitalWrite(toNicla, LOW);
+            if (pidDriveBool)
+              pidDrive();
+            }
+          }
+          motors.setSpeeds(0,0);      //the robot stops for half a second and then it will continue driving normally
+          delay(500);
+      }
+    }
   }
   return NiclaWantsToSend;
 }
 
+
+
 void readByte()
 {
+  Serial.print("Reading byte");
   digitalWrite(toNicla, HIGH); // Acknowledge Nicla
   delay(5); // Wait for stabilization
-  
   // Read a byte
   val = 0;
-  while (digitalRead(fromNicla) == LOW); // Wait for start bit
-
+  while (digitalRead(fromNicla) == LOW)
+  {
+   Serial.print("waiting for nicla");          ; // Wait for start bit
+  }
   delay(5 * 1.4); // Wait for the middle of the start bit
   digitalWrite(toNicla, LOW);
-
-  for (int offset = 0; offset < 9; offset++) {
+  for (int offset = 0; offset < 9; offset++) {  //9 bits value for a maximum value of 511
     if (digitalRead(fromNicla) == LOW) {
       val |= (1 << offset); // Set bit to 1
     }
-
     // delayMicroseconds(bitPeriod);
     delay(5);
   }
-
+  if (digitalRead(fromNicla) == LOW) {
+      pidDriveBool = false;                      //set pidDriveBool to false when another function is sent by the nicla
+    }
+    else{
+      pidDriveBool = true;                       //set pidDriveBool to true when nicla sends the error
+    }
+    delay(5);
 //  // Check if the last bit is 1 for two's complement
 //  if (val & (1 << 8)) {
 //    // If it is, convert to two's complement
 //    val = ((val | (1 << 15)) & ~(1 << 8));
 //  }
-
-  Serial.print("Received: ");
-  Serial.println(val);
+  //Serial.print("Received: ");
+  //Serial.println(val);
   display.clear();
   display.print(val);
 }
-
- 
